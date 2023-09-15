6a7b18e203eded24ad6fa9731d8a4b915f3ccf58
Kiera Johnson <16164505@edu.cbe.ab.ca>
2023-09-15 09:57:03 -0600

    finished insertion sort

--- insertion-sort.js
+++ insertion-sort.js
@@ -5,16 +5,29 @@ function insertionSort(array) {
 	 ** is bigger
 	 */
 
-	for (let i = array.length; i > 0; i--) {
-		if(array[i] < array[i-1]){
-			let small = array[i];
-			let big = array[i-1];
-			
-		}
-		
+	for (let i = 0; i < array.length; i++){
+		if(array[i+1] < array[i]){
+			// console.log(array);
+			swap(array, i+1, i);
+			for(let a = 0; a < i; a++){
+				if(array[i-a] < array[i-a-1]){
+					swap(array, i-a, i-a-1);
+				}
+			}	
+		}	
 	}
-
 	return array;
 }
+function swap(array, smallIndex, bigIndex){
+	let small = array[smallIndex];
+	let big = array[bigIndex];
+	array[smallIndex] = big;
+	array[bigIndex] = small;
+}
+
+// let arr = [8, 2, 3, 7, 5, 10]
+// console.log("Unsorted Array: " + arr);
+// let sortedArr = insertionSort(arr);
+// console.log("Sorted Array: " + sortedArr);
 
 module.exports = insertionSort;
